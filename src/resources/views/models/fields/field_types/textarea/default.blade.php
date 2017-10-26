cc5448fbd1ef2235cf950dc998b63df35de93a47
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-26 15:25:18 +0300

    Display errors in textarea

--- src/resources/views/models/fields/field_types/textarea/default.blade.php
+++ src/resources/views/models/fields/field_types/textarea/default.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group">
+<div class="form-group {{ $errors->has($field->name.'.'.$language->id) ? ' has-error' : '' }}">
 	<label class="col-sm-2" for="{{ $field->name }}-{{ $language->id }}">{{ $field->display_name }}</label>
 	<div class="col-sm-10">
 		<textarea 
@@ -10,6 +10,12 @@
 
 		@if($field->hint)
 			<div class="text-muted"><small>{{ $field->hint }}</small></div>
+		@endif
+
+		@if ($errors->has($field->name.'.'.$language->id))
+			<span class="help-block">
+				<strong>{{ $errors->first($field->name.'.'.$language->id) }}</strong>
+			</span>
 		@endif
 	</div>
 </div>
