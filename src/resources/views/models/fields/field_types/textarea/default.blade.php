1692b0cdb85e145d22cfd4d9f37fd52aa01a577c
Jaroslaw Goszowski <goszowski@gmail.com>
2017-10-26 15:25:50 +0300

    Old value in textarea

--- src/resources/views/models/fields/field_types/textarea/default.blade.php
+++ src/resources/views/models/fields/field_types/textarea/default.blade.php
@@ -6,7 +6,7 @@
 			class="form-control input-sm"
 			name="{{ $field->name }}[{{ $language->id }}]"
 			id="{{ $field->name }}-{{ $language->id }}"
-			>{{ old($field->name.'['.$language->id.']') ?: $value }}</textarea>
+			>{{ old($field->name.'.'.$language->id) ?: $value }}</textarea>
 
 		@if($field->hint)
 			<div class="text-muted"><small>{{ $field->hint }}</small></div>
