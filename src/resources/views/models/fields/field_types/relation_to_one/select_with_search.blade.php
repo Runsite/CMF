ff71b16b42f42db80ea7741c3ea01d45784017be
Jaroslaw Goszowski <goszowski@gmail.com>
2017-12-14 17:21:55 +0200

    Relation to one without search

--- src/resources/views/models/fields/field_types/relation_to_one/select_with_search.blade.php
+++ src/resources/views/models/fields/field_types/relation_to_one/select_with_search.blade.php
@@ -1,5 +1,3 @@
-
-
 <div class="form-group {{ $errors->has($field->name.'.'.$language->id) ? ' has-error' : '' }}">
 	<label class="col-sm-2" for="{{ $field->name }}-{{ $language->id }}">{{ $field->display_name }}</label>
 	<div class="col-sm-10">
