9b7857b14eb8b1fef56d0bee6be7a1d28148b651
Jaroslaw Goszowski <goszowski@gmail.com>
2018-01-22 13:24:15 +0200

    Replaed auth layout

--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -3,27 +3,15 @@
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-3 hidden-xs hidden-sm auth-image"></div>
-		<div class="col-md-9">
-			<div class="col-sm-4 col-sm-push-4 auth-body">
+		<div class="col-md-7">
+			<div class="col-sm-6 col-sm-push-3 auth-body">
 				<div class="auth-box">
 					<h1 class="h2"><a href="{{ route('login') }}">{{ config('app.name') }}</a></h1>
 					@yield('app')
 				</div>
 			</div>
 		</div>
+		<div class="col-md-5 hidden-xs hidden-sm auth-image"></div>
 	</div>
 </div>
-{{-- <div class="login-box">
-	<div class="login-logo">
-		<a href="{{ route('login') }}">{{ config('app.name') }}</a>
-	</div>
-	<!-- /.login-logo -->
-	<div class="login-box-body">
-		
-		@yield('app')
-	</div>
-	<!-- /.login-box-body -->
-</div> --}}
-
 @endsection
