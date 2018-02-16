08a1faf5c5060bce117f4e31d81cdcd7e520f1e1
Jaroslaw Goszowski <goszowski@gmail.com>
2018-02-16 15:28:20 +0200

    Added valid hint to field creating by templates

--- src/resources/langs/en/field_templates.php
+++ src/resources/langs/en/field_templates.php
@@ -3,25 +3,31 @@
 return [
 	'name' => [
 		'display_name' => 'Name',
+		'hint' => '',
 	],
 
 	'title' => [
 		'display_name' => 'SEO Title',
+		'hint' => '',
 	],
 
 	'description' => [
 		'display_name' => 'SEO Description',
+		'hint' => '',
 	],
 
 	'image' => [
 		'display_name' => 'Image',
+		'hint' => '',
 	],
 
 	'pubdate' => [
 		'display_name' => 'Date of publication',
+		'hint' => '',
 	],
 
 	'content' => [
 		'display_name' => 'Text',
+		'hint' => '',
 	],
 ];
