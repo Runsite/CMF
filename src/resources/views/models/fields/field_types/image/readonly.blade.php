1c6201a322e45df775325a4222a3dd8d3579ecaf
Jaroslaw Goszowski <goszowski@gmail.com>
2018-02-16 16:23:39 +0200

    Readonly image field

--- src/resources/views/models/fields/field_types/image/readonly.blade.php
+++ src/resources/views/models/fields/field_types/image/readonly.blade.php
@@ -1,5 +1,12 @@
-@extends('runsite::models.fields.field_types.base')
+<div class="form-group {{ $errors->has($field->name.'.'.$language->id) ? ' has-error' : '' }}">
+	<label class="col-sm-2" for="{{ $field->name }}-{{ $language->id }}">{{ $field->display_name }}</label>
+	<div class="col-sm-10">
+		<input type="hidden" name="{{ $field->name }}[{{ $language->id }}]">
 
-@section('field')
-    Field content
-@endsection
+		@if($value and $value->value)
+			<img src="{{ $value->min() }}" class="img-responsive">
+		@endif
+
+		
+	</div>
+</div>
