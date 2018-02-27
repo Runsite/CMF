c19003aecccb23588117d2b391a14d52c45e4f32
Jarosław Goszowski <goszowski@gmail.com>
2018-02-27 17:53:30 +0200

    Create ip_auth_limit.php

---
+++ publish/config/ip_auth_limit.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+	/* You can use array for allow to specific ips */
+	'allow_ips' => '*',
+	
+	/* You can use string "*" for disallow all ips */
+	'disallow_ips' => [
+		//
+	],
+];
