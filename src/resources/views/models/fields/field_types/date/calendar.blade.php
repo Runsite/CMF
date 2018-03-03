a04697e62656bfd990ad5312c83a5da9a464efff
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-03 10:58:34 +0200

    The ability to empty the date and datetime fields
    Co-Authored-By: Jarosław Goszowski <goszowski@gmail.com>

--- src/resources/views/models/fields/field_types/date/calendar.blade.php
+++ src/resources/views/models/fields/field_types/date/calendar.blade.php
@@ -1,12 +1,19 @@
 <div class="form-group {{ $errors->has($field->name.'.'.$language->id) ? ' has-error' : '' }}">
 	<label class="col-sm-2" for="{{ $field->name }}-{{ $language->id }}">{{ $field->display_name }}</label>
 	<div class="col-sm-10">
-		<input 
-			type="text" 
-			class="form-control input-sm input-date" 
-			name="{{ $field->name }}[{{ $language->id }}]" 
-			id="{{ $field->name }}-{{ $language->id }}" 
-			value="{{ old($field->name.'.'.$language->id) ?: $value }}">
+		<div class="input-group input-group-sm">
+			<input 
+				type="text" 
+				class="form-control input-sm input-date" 
+				name="{{ $field->name }}[{{ $language->id }}]" 
+				id="{{ $field->name }}-{{ $language->id }}" 
+				value="{{ old($field->name.'.'.$language->id) ?: $value }}">
+			<span class="input-group-btn">
+				<button class="btn btn-primary ripple" onclick="$(this).parent().parent().find('input').val('')" type="button">
+					<i class="fa fa-times"></i>
+				</button>
+			</span>
+		</div>
 
 		@if($field->hint)
 			<div class="text-muted"><small>{{ $field->hint }}</small></div>
