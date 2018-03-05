f678d93aac8116f177f90e9bd878ec1480a188b9
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-05 16:52:10 +0200

    Fixed bug: removing relations one to many
    Co-Authored-By: Jarosław Goszowski <goszowski@gmail.com>

--- src/resources/views/models/fields/field_types/relation_to_many/select_with_search.blade.php
+++ src/resources/views/models/fields/field_types/relation_to_many/select_with_search.blade.php
@@ -1,6 +1,9 @@
 <div class="form-group {{ $errors->has($field->name.'.'.$language->id) ? ' has-error' : '' }}">
 	<label class="col-sm-2" for="{{ $field->name }}-{{ $language->id }}">{{ $field->display_name }}</label>
 	<div class="col-sm-10">
+		
+		<input type="hidden" name="{{ $field->name }}[{{ $language->id }}]" value="">
+
 		<select 
 			class="form-control input-sm relation-to-many-search" 
 			style="width: 100%;" 
