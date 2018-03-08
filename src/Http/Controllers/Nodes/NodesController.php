a7c4fa24e9830446ab4153c0818b02167d24b284
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-08 14:57:41 +0200

    Fixed bug: redirectiong with success after creating new node
    Co-Authored-By: Jarosław Goszowski <goszowski@gmail.com>

--- src/Http/Controllers/Nodes/NodesController.php
+++ src/Http/Controllers/Nodes/NodesController.php
@@ -156,12 +156,12 @@ class NodesController extends BaseAdminController
 		if($request->has('create_next_one'))
 		{
 			return redirect()->route('admin.nodes.create', ['model_id'=>$model->id, 'parent_id'=>$node->baseNode->parent_id])
-				->with('succcess', trans('runsite::nodes.The node is created'))->with('create_next_one', true);
+				->with('success', trans('runsite::nodes.The node is created'))->with('create_next_one', true);
 		}
 
 		// Redirecting with success message
 		return redirect()->route('admin.nodes.edit', ['node'=>$parent_node, 'depended_model_id'=>$model->id])
-			->with('succcess', trans('runsite::nodes.The node is created'));
+			->with('success', trans('runsite::nodes.The node is created'));
 	}
 
 	/**
