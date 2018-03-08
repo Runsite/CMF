dd664c327fe82be7b97a241806351f7a4761e2de
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-08 15:25:49 +0200

    Added ckeditor config
    Co-Authored-By: Jarosław Goszowski <goszowski@gmail.com>

--- publish/public/asset/plugins/ckeditor/config.js
+++ publish/public/asset/plugins/ckeditor/config.js
@@ -10,4 +10,18 @@ CKEDITOR.editorConfig = function( config ) {
 	config.skin = 'office2013';
 	config.extraPlugins = 'mediaembed';
 	config.allowedContent = true;
+
+	config.toolbar_compact =
+	[
+		{ name: 'tools', items : [ 'Maximize', 'Source' ] },
+		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-'] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','-','RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-', '-','Blockquote',
+		'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
+		{ name: 'links', items : [ 'Link','Unlink' ] },
+		'/',
+		{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+		{ name: 'colors', items : [ 'TextColor','BGColor' ] },
+		{ name: 'insert', items : [ 'Image', 'Mediaembed', 'Table','HorizontalRule' ] },
+	];
 };
