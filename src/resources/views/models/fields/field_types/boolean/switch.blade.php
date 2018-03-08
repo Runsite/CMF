2837794f8d0d85af20337c9c5ddb6f9b669bdc1b
Jaroslaw Goszowski <goszowski@gmail.com>
2018-03-08 11:04:42 +0200

    Fixed boolean switch
    Co-Authored-By: Jarosław Goszowski <goszowski@gmail.com>

--- src/resources/views/models/fields/field_types/boolean/switch.blade.php
+++ src/resources/views/models/fields/field_types/boolean/switch.blade.php
@@ -5,7 +5,6 @@
 		<div class="runsite-checkbox">
 			<input 
 				type="checkbox" 
-				checked="checked" 
 				value="1"
 				id="{{ $field->name }}-{{ $language->id }}"
 				name="{{ $field->name }}[{{ $language->id }}]"
