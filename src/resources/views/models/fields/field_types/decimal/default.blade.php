63a535f9230fb7a1df436900d096dd2f917d8aa7
Jaroslaw Goszowski <goszowski@gmail.com>
2018-04-27 12:33:47 +0300

    Fixed attribure "for" in field controls

--- src/resources/views/models/fields/field_types/decimal/default.blade.php
+++ src/resources/views/models/fields/field_types/decimal/default.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label class="col-sm-2" for="">
+    <label class="col-sm-2" for="{{ $field->name }}-{{ $language->id }}">
     	{{ $field->display_name }}
     	@if($userCanReadModels)
 			<br><small class="text-muted" style="font-weight: normal">{{ $field->name }} <a href="{{ route('admin.models.fields.edit', ['model'=>$model, 'field'=>$field]) }}" class="text-red"><i class="fa fa-cog"></i></a></small>
